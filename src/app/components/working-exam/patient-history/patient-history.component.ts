0f16496e032c5ac74880d2b85123fef5f83ff20d
nicolasdumontnid <nicolas.dumont@telemis.com>
2025-08-29 12:27:56 +0200

    Ajouter bouton de fermeture et aligner les en-têtes

--- src/app/components/working-exam/patient-history/patient-history.component.ts
+++ src/app/components/working-exam/patient-history/patient-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailedExam } from '../../../../models/detailed-exam.interface';
 
@@ -12,4 +13,9 @@ import { DetailedExam } from '../../../../models/detailed-exam.interface';
 })
 export class PatientHistoryComponent {
   @Input() exam: DetailedExam | null = null;
+  @Output() close = new EventEmitter<void>();
+
+  onClose(): void {
+    this.close.emit();
+  }
 }
